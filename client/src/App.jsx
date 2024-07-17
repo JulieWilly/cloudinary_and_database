ab89408eac986c7ac29e3bc2cff794677a39b70a
Wilfred Muchire <logincoin40@gmail.com>
2024-07-17 12:01:53 +0300

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import "./App.css";
 import axios from "axios";
 import Img from "./component/Img";
+import { cat } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { keyframeInterval } from "@cloudinary/url-gen/actions/transcode";
 
 function App() {
   const [file, setFile] = useState("");
   const [image, setImage] = useState("");
   const [uploadedImg, setUploadedImg] = useState("");
+  const [getImage, setGetImage] = useState([]);
+
 
   const previewFiles = (file) => {
     const reader = new FileReader(); // read the file or image
@@ -34,7 +38,7 @@ function App() {
     e.preventDefault();
     try {
       const saveImg = await axios
-        .post("http://localhost:3001", {
+        .post("http://localhost:3001/upload", {
           image: image,
         })
         .catch((error) => console.log(error));
@@ -44,6 +48,8 @@ function App() {
         const uploadedImg = saveImg.data.data.public_id;
         console.log("id", uploadedImg);
         setUploadedImg(uploadedImg);
+    findImage();
+
       } catch (error) {
         console.log(error);
       }
@@ -51,7 +57,23 @@ function App() {
     } catch (error) {
       console.log(error);
     }
+
   };
+
+  // get the image from the database.
+  const findImage = async () => {
+    try{
+    const getData = await axios
+      .get("http://localhost:3001/image")
+      .catch((error) => console.log(error));
+    console.log('imagedata', getData.data.data);
+    setGetImage(getData.data.data);
+    } catch(error) {
+      console.log('2',error)
+    }
+  }
+
+
   return (
     <>
       <form onSubmit={(e) => handleSubmit(e)}>
@@ -69,6 +91,11 @@ function App() {
       <img src={image} alt="uploaded image should appear here." />
       {/* //prop drill to pass image to another component for transformations. */}
       <Img uploadedImg={uploadedImg} />
+
+      <p>image from database section </p>
+      {getImage.map((images, i) => (
+        <img  key={getImage.id} src={images.imgUrl} alt="image from database." />
+      ))}
     </>
   );
 }
